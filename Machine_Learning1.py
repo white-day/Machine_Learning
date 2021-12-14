b0085acd460dd105595423d6a6156d1d8dc3a4dc
white-day <66308339+white-day@users.noreply.github.com>
2021-12-14 21:52:49 +0800

    Add files via upload

---
+++ Machine_Learning1.py
@@ -0,0 +1,211 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import seaborn as sns
+# %matplotlib inline
+
+np.random.seed(2)
+
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
+import itertools
+
+from keras.utils.np_utils import to_categorical # convert to one-hot-encoding
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPool2D
+#from keras.optimizers import RMSprop
+from tensorflow.keras.optimizers import RMSprop
+from keras.preprocessing.image import ImageDataGenerator
+from keras.callbacks import ReduceLROnPlateau
+
+sns.set(style='white', context='notebook', palette='deep')
+
+# Load the data
+train = pd.read_csv("D:/software/digit/train.csv")
+test = pd.read_csv("D:/software/digit/test.csv")
+
+Y_train = train["label"]
+X_train = train.drop(labels = ["label"],axis = 1) 
+
+# free some space
+del train 
+
+g = sns.countplot(Y_train)
+Y_train.value_counts()
+
+X_train.isnull().any().describe()
+test.isnull().any().describe()
+
+X_train = X_train / 255.0
+test = test / 255.0
+
+X_train = X_train.values.reshape(-1,28,28,1)
+test = test.values.reshape(-1,28,28,1)
+
+# eg.  2 -> [0,0,1,0,0,0,0,0,0,0])
+Y_train = to_categorical(Y_train, num_classes = 10)
+
+
+random_seed = 2  #可去掉
+X_train, X_val, Y_train, Y_val = train_test_split(X_train, Y_train, test_size = 0.1, random_state=random_seed)
+
+# Some examples
+g = plt.imshow(X_train[0][:,:,0])
+
+model = Sequential()
+
+model.add(Conv2D(filters = 32, kernel_size = (5,5),padding = 'Same', 
+                 activation ='relu', input_shape = (28,28,1)))
+model.add(Conv2D(filters = 32, kernel_size = (5,5),padding = 'Same', 
+                 activation ='relu'))
+model.add(MaxPool2D(pool_size=(2,2)))
+#dropout用于正则化项，随机丢失一些节点，防止网络过拟合
+model.add(Dropout(0.25))
+
+
+model.add(Conv2D(filters = 64, kernel_size = (3,3),padding = 'Same', 
+                 activation ='relu'))
+model.add(Conv2D(filters = 64, kernel_size = (3,3),padding = 'Same', 
+                 activation ='relu'))
+model.add(MaxPool2D(pool_size=(2,2), strides=(2,2)))
+model.add(Dropout(0.25))
+
+# 将特征图转换成1D向量
+model.add(Flatten())
+model.add(Dense(256, activation = "relu"))
+model.add(Dropout(0.5))
+model.add(Dense(10, activation = "softmax"))
+
+optimizer = RMSprop(lr=0.001, rho=0.9, epsilon=1e-08, decay=0.0)
+
+model.compile(optimizer = optimizer , loss = "categorical_crossentropy", metrics=["accuracy"])
+
+learning_rate_reduction = ReduceLROnPlateau(monitor='val_accuracy', 
+                                            patience=3, 
+                                            verbose=1, 
+                                            factor=0.5, 
+                                            min_lr=0.00001)
+epochs = 1 
+batch_size = 86
+
+# 使用数据增强算法
+datagen = ImageDataGenerator(
+        featurewise_center=False, 
+        samplewise_center=False, 
+        featurewise_std_normalization=False,  
+        samplewise_std_normalization=False,  
+        zca_whitening=False,  
+        rotation_range=10, 
+        zoom_range = 0.1, 
+        width_shift_range=0.1, 
+        height_shift_range=0.1, 
+        horizontal_flip=False,  
+        vertical_flip=False)  
+
+datagen.fit(X_train)
+
+history = model.fit(datagen.flow(X_train,Y_train, batch_size=batch_size),
+                              epochs = epochs, validation_data = (X_val,Y_val),
+                              verbose = 2, steps_per_epoch=X_train.shape[0] // batch_size
+                              , callbacks=[learning_rate_reduction])
+
+fig, ax = plt.subplots(2,1)
+ax[0].plot(history.history['loss'], color='b', label="Training loss")
+ax[0].plot(history.history['val_loss'], color='r', label="validation loss",axes =ax[0])
+legend = ax[0].legend(loc='best', shadow=True)
+
+ax[1].plot(history.history['accuracy'], color='b', label="Training accuracy")
+ax[1].plot(history.history['val_accuracy'], color='r',label="Validation accuracy")
+legend = ax[1].legend(loc='best', shadow=True)
+
+history.history.keys()
+
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, cm[i, j],
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+
+# Predict the values from the validation dataset
+Y_pred = model.predict(X_val)
+# Convert predictions classes to one hot vectors 
+Y_pred_classes = np.argmax(Y_pred,axis = 1) 
+# Convert validation observations to one hot vectors
+Y_true = np.argmax(Y_val,axis = 1) 
+# compute the confusion matrix
+confusion_mtx = confusion_matrix(Y_true, Y_pred_classes) 
+# plot the confusion matrix
+plot_confusion_matrix(confusion_mtx, classes = range(10)) 
+
+# 得到的是bool值，每一个数据是否预测正确
+errors = (Y_pred_classes - Y_true != 0)
+# 错误预测的预测值
+Y_pred_classes_errors = Y_pred_classes[errors]
+# 错误预测上各个数字预测概率
+Y_pred_errors = Y_pred[errors]
+# 真实值
+Y_true_errors = Y_true[errors]
+# 对应的图像
+X_val_errors = X_val[errors]
+
+def display_errors(errors_index,img_errors,pred_errors, obs_errors):
+    """ This function shows 6 images with their predicted and real labels"""
+    n = 0
+    nrows = 2
+    ncols = 3
+    fig, ax = plt.subplots(nrows,ncols,sharex=True,sharey=True)
+    for row in range(nrows):
+        for col in range(ncols):
+            error = errors_index[n]
+            ax[row,col].imshow((img_errors[error]).reshape((28,28)))
+            ax[row,col].set_title("Predicted label :{}\nTrue label :{}".format(pred_errors[error],obs_errors[error]))
+            n += 1
+
+Y_pred_errors_prob = np.max(Y_pred_errors,axis = 1)
+true_prob_errors = np.diagonal(np.take(Y_pred_errors, Y_true_errors, axis=1))
+
+delta_pred_true_errors = Y_pred_errors_prob - true_prob_errors
+
+sorted_dela_errors = np.argsort(delta_pred_true_errors)
+
+# 顶部6条错误
+most_important_errors = sorted_dela_errors[-6:]
+
+#展示顶部6条错误
+display_errors(most_important_errors, X_val_errors, Y_pred_classes_errors, Y_true_errors)
+
+# predict results
+results = model.predict(test)
+
+# select the indix with the maximum probability
+results = np.argmax(results,axis = 1)
+
+results = pd.Series(results,name="Label")
+
+submission = pd.concat([pd.Series(range(1,28001),name = "ImageId"),results],axis = 1)
+
+submission.to_csv("cnn_mnist_datagen.csv",index=False)
+
